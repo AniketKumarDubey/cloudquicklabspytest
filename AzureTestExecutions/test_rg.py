516a3e9be87737348227ae42e29869d4215d1b70
AniketKumarDubey <114729222+AniketKumarDubey@users.noreply.github.com>
2024-11-04 14:24:12 +0000

    Update test_rg.py
    Modified correct Subscription id

--- AzureTestExecutions/test_rg.py
+++ AzureTestExecutions/test_rg.py
@@ -2,7 +2,7 @@ from azure.identity import AzureCliCredential
 from azure.mgmt.resource import ResourceManagementClient
 
 # Replace these values with your Azure subscription ID, resource group, and storage account name
-SUBSCRIPTION_ID = '063965e0-d141-48d4-b8bd-0dfdc0dc00ff'
+SUBSCRIPTION_ID = '86ac5624-f280-4b8f-8fd7-520872422e29'
 RESOURCE_GROUP = 'demorg'
 
 from AzureTestCases.azure_rg_tests import azure_check_if_rg_exists
